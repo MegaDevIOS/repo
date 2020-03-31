db2c10a26a406f4318c530ecbaa13bd7e0fc0704
Mega <cooljj666@gmail.com>
2020-03-31 21:12:34 +0200

    updayte

--- update.sh
+++ update.sh
@@ -3,3 +3,5 @@ dpkg-scanpackages debs > Packages
 cp Packages Packages-copy
 bzip2 Packages-copy
 mv Packages-copy.bz2 Packages.bz2
+bzip2 -c Packages > Packages.bz2
+gzip  -c Packages > Packages.gz
