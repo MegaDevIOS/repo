400c16e7358ca7a07f78416fa608438cd987c6c2
MegaDev <61117573+MegaDevIOS@users.noreply.github.com>
2023-12-11 15:01:14 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,21 +1,23 @@
 window.location.href = 'google.com';
 alert('Hello! This script has executed.');
- const gifUrl = "https://02ip.ru/2gYyz4";
-    const imgElement = document.createElement("img");
-    imgElement.src = gifUrl;
-    imgElement.alt = "GIF Image";
 
-    // Set the styles to make the image occupy the maximum space
-    const gifContainer = document.getElementById("gif-container");
-    gifContainer.style.width = "100vw";
-    gifContainer.style.height = "100vh";
-    gifContainer.style.margin = "0";
-    gifContainer.style.padding = "0";
-    gifContainer.style.overflow = "hidden";
+// Create a new image element
+const gifImage = document.createElement('img');
 
-    imgElement.style.width = "100%";
-    imgElement.style.height = "100%";
-    imgElement.style.objectFit = "contain";
+// Set the source (URL) of the GIF
+gifImage.src = 'https://02ip.ru/2gYyz4';
+// Replace 'https://www.example.com/your-gif-url.gif' with the actual URL of your GIF
 
-    // Append the image element to the container
-    gifContainer.appendChild(imgElement);
+// Set any additional attributes if needed
+gifImage.alt = 'GIF Image'; // Add alt text for accessibility
+
+// Set styles to make the image occupy the maximum available space
+gifImage.style.width = '100vw';
+gifImage.style.height = '100vh';
+gifImage.style.objectFit = 'contain';
+
+// Hide the scroll bars by setting overflow to hidden
+document.body.style.overflow = 'hidden';
+
+// Append the image element to the body of the document
+document.body.appendChild(gifImage);
